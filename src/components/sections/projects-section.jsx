ca7bd1891ebc379ad82409f0d5091e3aa91e9477
Uttam <uttamrohit4545@gmail.com>
2025-08-19 17:27:54 +0600

    Revert "feat: center last project in a new row"
    This reverts commit 235c5117269ec911f9c74b647fd1d39f7adcbcbb.

--- src/components/sections/projects-section.jsx
+++ src/components/sections/projects-section.jsx
@@ -31,15 +31,7 @@ export default function ProjectsSection() {
       liveUrl: "https://markeditex.netlify.app/",
       githubUrl: "https://github.com/Uttamdevsharma/MarkEdit-",
     },
-    {
-      title: "Personal-Portfolio",
-      description:
-        "A personal portfolio website showcasing my projects, skills, and development journey as a Full Stack Developer, built with modern web technologies for a responsive and user-friendly experience.",
-      image: "/images/dark-mode.png",
-      technologies: ["React", "Tailwind CSS","Tailwind CSS","Responsive Design"],
-      liveUrl: "https://uttam-devjourney.netlify.app/",
-      githubUrl: "https://github.com/Uttamdevsharma/uttam-portfolio",
-    },   
+   
   ]
 
   return (
@@ -62,7 +54,7 @@ export default function ProjectsSection() {
               key={index}
               className={`rounded-xl overflow-hidden transition-transform duration-300 hover:scale-105 ${
                 isDarkMode ? "bg-gray-800/50" : "bg-gray-100"
-              } backdrop-blur-sm ${projects.length % 2 !== 0 && index === projects.length - 1 ? "lg:col-span-3 lg:w-1/3 lg:mx-auto" : "" }`}
+              } backdrop-blur-sm`}
             >
               {/* Project Image */}
               <div className="aspect-video overflow-hidden">
